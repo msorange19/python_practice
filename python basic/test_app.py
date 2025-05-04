2646be184c3f86370c4fb8fd1d0ef32588e87c3f
Muhaiminul Islam <muhaiminulislam@Muhaiminuls-MacBook-Air.local>
2025-05-04 22:39:28 +0600

    commit day 1

---
+++ python basic/test_app.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_example_function():
+    assert example_function() == expected_value
+
+def test_another_function():
+    assert another_function(param) == expected_outcome
